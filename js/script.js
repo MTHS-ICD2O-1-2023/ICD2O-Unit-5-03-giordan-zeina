6ab11f6e6b1397d8e78f8c080ee2c990232daa1a
Giordan Zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-04-25 18:47:00 +0000

    good css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Giordan Zeina All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+
+  const age = parseInt(document.getElementById('age-entered').value)
+
+  if (age >= 18) {
+
+    document.getElementById('answer').innerHTML = 'You can see an R rated movie alone.'
+
+  } else if (age >= 13) {
+
+    document.getElementById('answer').innerHTML = 'You can see a PG-13 movie alone.'
+
+  } else if (age >= 9) {
+
+    document.getElementById('answer').innerHTML = 'You can see a G or PG rated movie alone.'
+
+  } else if (age >= 6) {
+
+    document.getElementById('answer').innerHTML = 'You can see a G rated movie alone.'
+
+  }
+
+}
